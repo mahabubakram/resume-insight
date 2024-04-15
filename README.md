28704b397a8f33c40381b5f9086cfb6a0e3bf68a
mahabubakram <m.akram040@gmail.com>
2024-04-15 19:10:22 +0200

    loki added and logs created

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Swagger UI:
 http://127.0.0.1:8000/docs
+
+
+## Docker commands:
+sudo chmod 666 /var/run/docker.sock
--- logger.py
+++ logger.py
@@ -1,13 +1,36 @@
 import logging
-#https://prometheus.io/docs/introduction/faq/#how-to-feed-logs-into-prometheus
+import sys
+from multiprocessing import Queue
+
+from logging_loki import LokiQueueHandler
+
+# https://prometheus.io/docs/introduction/faq/#how-to-feed-logs-into-prometheus
+
+loki_logs_handler = LokiQueueHandler(
+    Queue(-1),
+    url="http://loki:3100/loki/api/v1/push",
+    tags={"application": "fastapi"},
+    version="1",
+)
+
 
 def get_logger(name, level=logging.INFO) -> logging.Logger:
-    logger = logging.getLogger(name)
+    logger = logging.getLogger("uvicorn.access")
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # add filehandler
-    fh = logging.FileHandler('logs.log')
-    fh.setLevel(level)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    # create handlers
+    stream_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler('logs.log')
+
+    # set fromatters
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # set level
+    stream_handler.setLevel(level)
+    file_handler.setLevel(level)
+
+    # add handlers to the logger
+    logger.handlers = [stream_handler, file_handler, loki_logs_handler]
+
     return logger
